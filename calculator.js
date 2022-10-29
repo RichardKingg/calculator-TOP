5a22df6eb15364836baf4679bbca6828bf3d7dae
RichardKingg <ricardo.elizondo.jau@gmail.com>
2022-10-29 18:19:19 -0500

    Declare variables from index html

---
+++ calculator.js
@@ -0,0 +1,35 @@
+// Head of container
+const calcBody = document.getElementById("calcBody");
+const calcHead = document.getElementById("calcHead");
+const calcDisp = document.getElementById("calcDisp");
+
+// Button container
+const calcBtnCont = document.getElementById("calcBtnCont");
+const calcRow = document.getElementsByClassName("calcRow");
+
+// Buttons
+const buttons = document.querySelectorAll("button");
+const mrc = document.getElementById("mrc");
+const mNeg = document.getElementById("m-");
+const mPlus = document.getElementById("m+");
+const sqRoot = document.getElementById("sqRoot");
+const sign = document.getElementById("sign");
+const onReset = document.getElementById("onReset");
+const mu = document.getElementById("mu");
+const percent = document.getElementById("percent");
+const divideBtn = document.getElementById("divideBtn");
+const btn7 = document.getElementById("btn7");
+const btn8 = document.getElementById("btn8");
+const btn9 = document.getElementById("btn9");
+const multBtn = document.getElementById("multBtn");
+const btn4 = document.getElementById("btn4");
+const btn5 = document.getElementById("btn5");
+const btn6 = document.getElementById("btn6");
+const minusBtn = document.getElementById("minusBtn");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const ceroBtn = document.getElementById("ceroBtn");
+const floatBtn = document.getElementById("floatBtn");
+const equalBtn = document.getElementById("equalBtn");
+const plusBtn = document.getElementById("plusBtn");
